accf24926f480df8d4a074bf2861e92a92059a27
John Schomp <john@grayquarter.com>
2018-01-03 08:24:59 -0800

    no BTRC check for testing records

--- SCRIPTS/EVENT/ASA;LICENSES!~!~!APPLICATION.js
+++ SCRIPTS/EVENT/ASA;LICENSES!~!~!APPLICATION.js
@@ -8,6 +8,8 @@
 include("SAVE_BUSINESS_INFO_TO_RECORD");
 // End script to copy Business contact information (Business Name and Address) to record
 
-// Check matching FEIN or SSN for this BTRC
-include("CHECK_SSN_FEIN_MATCH");
+// Check matching FEIN or SSN for this BTRC, not for testing records
+if (!appMatch("Licenses/Cannabis/Testing/Application")) {
+	include("CHECK_SSN_FEIN_MATCH");
+	}
 // end check for fein/ssn
