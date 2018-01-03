9754ae1bbbb13184994d0fe7f66c70fae3dc4962
John Schomp <john@grayquarter.com>
2018-01-03 08:38:33 -0800

    BTRC Logic

--- SCRIPTS/EVENT/ASA;LICENSES!~!~!APPLICATION.js
+++ SCRIPTS/EVENT/ASA;LICENSES!~!~!APPLICATION.js
@@ -7,3 +7,9 @@
 // Begin script to copy Business contact information (Business Name and Address) to record
 include("SAVE_BUSINESS_INFO_TO_RECORD");
 // End script to copy Business contact information (Business Name and Address) to record
+
+// Check matching FEIN or SSN for this BTRC, not for testing records
+if (!appMatch("Licenses/Cannabis/Testing/Application")) {
+	include("CHECK_SSN_FEIN_MATCH");
+	}
+// end check for fein/ssn
