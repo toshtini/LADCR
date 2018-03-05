fc03f58cc3de6c338cd8605f1ccd321a05e38490
grayquarter <john@grayquarter.com>
2018-03-04 20:39:38 -0800

    deadline code

---
+++ SCRIPTS/EVENT/ASB;LICENSES!~!~!APPLICATION.js
@@ -0,0 +1,5 @@
+if (new Date() > new Date("03/03/2018")) {
+	cancel = true ; 
+	showMessage = true;
+	comment("Your application is not submitted.   All applications must be received by 11:59PM on March 4, 2018");
+	}
