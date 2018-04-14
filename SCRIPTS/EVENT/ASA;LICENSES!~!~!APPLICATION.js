4dac160812271f13306c18913212f8e19860006c
John Schomp <john@grayquarter.com>
2018-04-13 19:29:19 -0700

    fix issue

--- SCRIPTS/EVENT/ASA;LICENSES!~!~!APPLICATION.js
+++ SCRIPTS/EVENT/ASA;LICENSES!~!~!APPLICATION.js
@@ -8,16 +8,7 @@
 include("SAVE_BUSINESS_INFO_TO_RECORD");
 // End script to copy Business contact information (Business Name and Address) to record
 
-// Check matching FEIN or SSN for this BTRC, not for testing records
-if (!appMatch("Licenses/Cannabis/Testing/Application")) {
-	include("CHECK_SSN_FEIN_MATCH");
-	}
-<<<<<<< HEAD
-// end check for fein/ssn
-=======
-// end check for fein/ssn
-
 // Assess fees
 include("ASSESS_BUS_APP_FEES");
 // end assess fees
->>>>>>> Dev
+
