63c7c1b7391604818d5f1e5ce34a1536ab2c46f3
Gregg Hess <ghess@accela.com>
2018-05-15 12:20:53 -0700

    Update ASA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPEAL PETITION.js

--- SCRIPTS/EVENT/ASA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPEAL PETITION.js
+++ SCRIPTS/EVENT/ASA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPEAL PETITION.js
@@ -1,2 +1,6 @@
 // Invoice Appela Petition Fee
 updateFee("CAN_APP_01","CAN_APP_PET","FINAL",1,"Y");
+// Send Fee Notice
+if (publicUser == false) {
+  include("SEND_APPEAL_PETITION_FEE_NOTICE");
+}
