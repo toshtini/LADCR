0e6d8709c08b4d005bd620bb6aab5af96be35432
Gregg Hess <ghess@accela.com>
2018-05-15 12:22:23 -0700

    Update CTRCA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPEAL PETITION.js

--- SCRIPTS/EVENT/CTRCA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPEAL PETITION.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPEAL PETITION.js
@@ -1,3 +1,7 @@
 if (feeAmountAll(capId,"NEW") > 0 && balanceDue == 0) {
 	invoiceFeeAllNew(capId);
 }
+// Send Fee Notice
+if (publicUser == true) {
+  include("SEND_APPEAL_PETITION_FEE_NOTICE");
+}
