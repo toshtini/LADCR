9c756df64aca2b76c87bb5de9029b376635ef049
Gregg Hess <ghess@accela.com>
2018-05-17 17:37:39 -0700

    Update CTRCA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPEAL PETITION.js

--- SCRIPTS/EVENT/CTRCA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPEAL PETITION.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!CANNABIS!APPLICATION AMENDMENT!APPEAL PETITION.js
@@ -1,3 +1,13 @@
+//Begin script to link amendment to application when user Defers Payment in ACA
+var vParentId;
+vParentId = "" + aa.env.getValue("ParentCapID");
+if (vParentId == false || vParentId == null || vParentId == "") {
+     var vAppId = getAppSpecific("Application ID");
+     if (vAppId != "" && vAppId != null) {
+           addParent(vAppId);
+     }
+}
+//End script to link amendment to application when user Defers Payment in ACA
 if (feeAmountAll(capId,"NEW") > 0 && balanceDue == 0) {
 	invoiceFeeAllNew(capId);
 }
