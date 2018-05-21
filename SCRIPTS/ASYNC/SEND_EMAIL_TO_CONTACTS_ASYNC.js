bbbde632692a16e7bbb315fd8ef1ef7edab592d3
Gregg Hess <ghess@accela.com>
2018-05-21 10:59:52 -0700

    Update SEND_EMAIL_TO_CONTACTS_ASYNC.js

--- SCRIPTS/ASYNC/SEND_EMAIL_TO_CONTACTS_ASYNC.js
+++ SCRIPTS/ASYNC/SEND_EMAIL_TO_CONTACTS_ASYNC.js
@@ -9,6 +9,7 @@ var vRParams = aa.env.getValue("vRParams");
 var vChangeReportName = aa.env.getValue("vChangeReportName");
 var capId = aa.env.getValue("CapId");
 var vAddAdHocTask = aa.env.getValue("vAddAdHocTask");
+var vContactCapId = aa.env.getValue("vContactCapId");
 
 aa.print("2) sendEmailToContactTypes: " + sendEmailToContactTypes);
 aa.print("3) emailTemplate: " + emailTemplate);
@@ -155,10 +156,14 @@ else {
 		conEmail = null;
 		peopTemp = null;
 		//logDebug("          Searching for " + conTypeArray[z]);
+		//Determine capId from which to get contacts
+		if (vContactCapId == null || vContactCapId == "" || vContactCapId == false){
+			vContactCapId = capId;
+		}
 		if (conType == "Primary") {
-			vConObjArry = getContactObjsByCap_BCC(capId);
+			vConObjArry = getContactObjsByCap_BCC(vContactCapId);
 		} else {
-			vConObjArry = getContactObjsByCap_BCC(capId, conTypeArray[z]);
+			vConObjArry = getContactObjsByCap_BCC(vContactCapId, conTypeArray[z]);
 		}
 		for (x in vConObjArry) {
 			vConObj = vConObjArry[x];
