0e3375d03819affabc870bfd5e14cd7f6ddfdc96
Gregg Hess <ghess@accela.com>
2018-06-13 09:59:29 -0700

    Update SEND_INCOMPLETE_APPLICATION_NOTICE.js

--- SCRIPTS/LADCR EVENT SCRIPT/SEND_INCOMPLETE_APPLICATION_NOTICE.js
+++ SCRIPTS/LADCR EVENT SCRIPT/SEND_INCOMPLETE_APPLICATION_NOTICE.js
@@ -3,6 +3,7 @@ if ((wfTask == "Initial Review" || wfTask == "Review") && wfStatus == "Additiona
 	addParameter(vEParams, "$$LicenseType$$", appTypeAlias);
 	addParameter(vEParams, "$$ExpirationDate$$", dateAdd(null, 60));
 	addParameter(vEParams, "$$ApplicationID$$", capIDString);
+	addParameter(vEParams, "$$businessName$$", capName);
 	var vRParams = aa.util.newHashtable();
 	addParameter(vRParams, "p1Value", capIDString);
 	emailContacts_BCC("All", "DCA INCOMPLETE APPLICATION NOTICE", vEParams, "Incomplete Application Letter", vRParams);
