ae87ead5e6531810b4828c2bb19d8fbfc70f4a1d
grayquarter <john@grayquarter.com>
2018-12-19 10:47:47 -0800

    update Notify

--- SCRIPTS/LADCR EVENT SCRIPT/NOTIFY_ABANDONED_APP.js
+++ SCRIPTS/LADCR EVENT SCRIPT/NOTIFY_ABANDONED_APP.js
@@ -5,5 +5,6 @@ addParameter(vEParams, "$$ExpirationDate$$", dateAdd(null, 60));
 addParameter(vEParams, "$$ApplicationID$$", capIDString);
 addParameter(vEParams, "$$businessName$$", capName);
 addParameter(vEParams, "$$todaysDate$$", dateAdd(null,0));
+addParameter(vEParams, "$$address$$", getAddressInALine());
 emailContacts_BCC("All", "LADCR NOTICE OF APPLICATION ABANDONMENT", vEParams, "", "");
 
