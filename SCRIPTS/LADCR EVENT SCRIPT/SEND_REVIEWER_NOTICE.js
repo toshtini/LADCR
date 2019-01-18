d40cb77fc3519014e313359cd5d59b0a6fab32c6
Gregg Hess <ghess@accela.com>
2019-01-18 14:50:20 -0800

    Create SEND_REVIEWER_NOTICE.js

---
+++ SCRIPTS/LADCR EVENT SCRIPT/SEND_REVIEWER_NOTICE.js
@@ -0,0 +1,35 @@
+// send email notice to task reviewer if supervisor returns it for further review
+// use in WTU, assumes wfTask and wfStatus
+
+	if (wfStatus == "Needs Review") {
+		//Determine task to be reviewed based on task submitted
+		var reviewTask = "";
+		if (wfTask == "Supervisor Premise Review") reviewTask = "Premise Review"; else
+		if (wfTask == "Supervisor Owner Review") reviewTask = "Owner Review"; else
+		if (wfTask == "Supervisor Social Equity Review") reviewTask = "Social Equity Review"; else
+		if (wfTask == "Supervisor Temp Review") reviewTask = "Premises Temp Review"; 
+	  
+		//Get task reviewer and send notice
+		if (reviewTask != "") {
+			var assignedStaffUser = getTaskAssignedStaff(reviewTask);
+			aa.print("assignedStaffUser = " + assignedStaffUser);
+
+			var actionByUser = getTaskActionBy(reviewTask);
+			aa.print("actionByUser = " + actionByUser);
+			
+			var iNameResult  = aa.person.getUser(actionByUser);
+			if (!iNameResult.getSuccess())
+			{ 
+				aa.print("**ERROR retrieving  user model " + actionByUser + " : " + iNameResult.getErrorMessage()) ; 
+				//return false ; 
+			} else {
+				var iName = iNameResult.getOutput();
+				aa.print("getDeptOfUser = " +  iName.getDeptOfUser() + ", getUserID = " + iName.getUserID() + ", getEmail = " + iName.getEmail());
+
+				emailSubject = "Civic Platform Task Needs Review";
+				emailMessage = "Supervisor returned your task '" + reviewTask + "' for further review. Please check the application.";
+				email(iName.getEmail(),"dcrlicensing@lacity.org",emailSubject,emailMessage);
+				
+			}
+		}
+	}
