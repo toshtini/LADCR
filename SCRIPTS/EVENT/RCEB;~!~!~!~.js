98f896c56f87624f2e69b9a8e8c6b9aed53fca55
grayquarter <john@grayquarter.com>
2019-05-20 11:56:14 -0700

    berfor

--- SCRIPTS/EVENT/RCEB;~!~!~!~.js
+++ SCRIPTS/EVENT/RCEB;~!~!~!~.js
@@ -2,7 +2,7 @@
 if (publicUser) {
 	var people = aa.people.getPeople(ContactModel.getContactSeqNumber()).getOutput();
 	var seStatus = people.getSalutation();
-	if matches(seStatus,"Pending DCR Review","Not Eligible","SEP Tier 1 and Tier 2 Eligibility Verified","SEP Tier 2 Eligibility Verified","SEP Tier 1 Eligibility Verified") {
+	if (matches(seStatus,"Pending DCR Review","Not Eligible","SEP Tier 1 and Tier 2 Eligibility Verified","SEP Tier 2 Eligibility Verified","SEP Tier 1 Eligibility Verified")) {
 		cancel = true;
 		showMessage = true;
 		comment("No changes can be made to your profile at this time");
