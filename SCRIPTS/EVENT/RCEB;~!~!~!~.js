9248c0eec24fec790dcbf66facf117935b36b7a4
grayquarter <john@grayquarter.com>
2019-05-20 11:58:12 -0700

    before

--- SCRIPTS/EVENT/RCEB;~!~!~!~.js
+++ SCRIPTS/EVENT/RCEB;~!~!~!~.js
@@ -2,6 +2,7 @@
 if (publicUser) {
 	var people = aa.people.getPeople(ContactModel.getContactSeqNumber()).getOutput();
 	var seStatus = people.getSalutation();
+	logDebug("seStatus is " + seStatus);
 	if (matches(seStatus,"Pending DCR Review","Not Eligible","SEP Tier 1 and Tier 2 Eligibility Verified","SEP Tier 2 Eligibility Verified","SEP Tier 1 Eligibility Verified")) {
 		cancel = true;
 		showMessage = true;
