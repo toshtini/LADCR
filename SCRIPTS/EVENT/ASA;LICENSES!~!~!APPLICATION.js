cb803ea68b3b354122b85a4deb85254c278aa1a9
grayquarter <john@grayquarter.com>
2019-07-02 11:38:27 -0700

    no fees on renewal

--- SCRIPTS/EVENT/ASA;LICENSES!~!~!APPLICATION.js
+++ SCRIPTS/EVENT/ASA;LICENSES!~!~!APPLICATION.js
@@ -1,13 +1,23 @@
-include("SAVE_BUSINESS_INFO_TO_RECORD");	
-// End script to copy Business contact information (Business Name and Address) to record	// End script to copy Business contact information (Business Name and Address) to record
+// Begin associated forms for owner data
+//
+// EWYLAM -- turned off associated forms on the record, so I disabled this script (JHS)
+//include("DO_ASSOCIATED_FORM_OWNER_SUB");
+// End associated forms for owner data
 
+// Begin script to copy Business contact information (Business Name and Address) to record
+include("SAVE_BUSINESS_INFO_TO_RECORD");
+// End script to copy Business contact information (Business Name and Address) to record
 
- // Check matching FEIN or SSN for this BTRC, not for testing records	
-if (!appMatch("Licenses/Cannabis/Testing/Application")) {	
-	include("CHECK_SSN_FEIN_MATCH");	
-	}	
-// end check for fein/ssn	
+// Check matching FEIN or SSN for this BTRC, not for testing records
+if (!appMatch("Licenses/Cannabis/Testing/Application")) {
+	include("CHECK_SSN_FEIN_MATCH");
+}
+// end check for fein/ssn
 
- // Assess fees	// Assess fees
-include("ASSESS_BUS_APP_FEES");
+// Assess fees
+if (AInfo["Is this a Renewal?"] && AInfo["Is this a Renewal?"].substr(0, 1).toUpperCase().equals("Y")) {
+	// skip fees
+} else {
+	include("ASSESS_BUS_APP_FEES");
+}
 // end assess fees
