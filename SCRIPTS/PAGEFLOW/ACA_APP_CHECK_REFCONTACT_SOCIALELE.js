f2ea749498e54abf366911480ba5447f7020c8c6
Don Bates <39094801+dbates99@users.noreply.github.com>
2019-07-18 15:30:37 -0300

    db

--- SCRIPTS/PAGEFLOW/ACA_APP_CHECK_REFCONTACT_SOCIALELE.js
+++ SCRIPTS/PAGEFLOW/ACA_APP_CHECK_REFCONTACT_SOCIALELE.js
@@ -157,7 +157,7 @@ try {
 	
 	cancel = true;
 	showMessage = true;
-	comment("Here");
+	comment("Here2");
 		
 } catch (err) { logDebug(err)	}
 
@@ -211,3 +211,17 @@ function getContactByType4ACA(conType) {
 	}
 	return false;
 }
+
+function getRefContactForPublicUser(userSeqNum) {
+	contractorPeopleBiz = aa.proxyInvoker.newInstance("com.accela.pa.people.ContractorPeopleBusiness").getOutput();
+	userList = aa.util.newArrayList();
+	userList.add(aa.util.parseLong(userSeqNum));
+	peopleList = contractorPeopleBiz.getContractorPeopleListByUserSeqNBR(aa.getServiceProviderCode(), userList); 
+	if (peopleList != null) {
+		peopleArray = peopleList.toArray();
+		if (peopleArray.length > 0)
+			return peopleArray[0];
+	}
+	return null;
+}
+
