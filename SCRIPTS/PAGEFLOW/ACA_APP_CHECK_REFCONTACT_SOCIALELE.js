50d26d8fade1bf1c8f2e39cae46955043780edfc
Don Bates <39094801+dbates99@users.noreply.github.com>
2019-07-18 16:32:30 -0300

    db

--- SCRIPTS/PAGEFLOW/ACA_APP_CHECK_REFCONTACT_SOCIALELE.js
+++ SCRIPTS/PAGEFLOW/ACA_APP_CHECK_REFCONTACT_SOCIALELE.js
@@ -141,7 +141,7 @@ logDebug("balanceDue = " + balanceDue);
 // page flow custom code begin
 
 try {
-    showDebug = true;
+    //showDebug = true;
     logDebug(publicUserID)
     var pSeqNumber = publicUserID.replace("PUBLICUSER","");  
     logDebug(pSeqNumber)
@@ -157,7 +157,8 @@ try {
 	if (contrPeopleModel != null) {
         refNum = contrPeopleModel.getContactSeqNumber();
         logDebug("refNum " + refNum)
-	    var refConResult = aa.people.getPeople(refNum);
+        var refConResult = aa.people.getPeople(refNum);
+        logDebug("refConResult.getSuccess  " + refConResult.getSuccess())
 		if (refConResult.getSuccess()) {
 			if (refPeopleModel != null) {
 				
