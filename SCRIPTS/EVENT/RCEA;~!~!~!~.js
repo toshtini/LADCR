7ae655faf40a38676b24d46bf1814693f256a2b1
Don Bates <39094801+dbates99@users.noreply.github.com>
2019-07-19 10:24:33 -0300

    db

--- SCRIPTS/EVENT/RCEA;~!~!~!~.js
+++ SCRIPTS/EVENT/RCEA;~!~!~!~.js
@@ -45,8 +45,7 @@ if (!publicUser)
 	addParameter(vEParams, "$$ApplicationID$$", capIDString);
 	if(afterEditSocialEquity != beforeEditSocialEquity)
 		{
-		emailContacts_BCC("All", "LACDR_SOCIAL_EQUITY_STATUS_CHANGE_ALERT", vEParams, null, null)
-		}
+		emailAsync_BCC(refContactEmail, "LACDR_SOCIAL_EQUITY_STATUS_CHANGE_ALERT", vEParams, null, null, null, null)
 	}
 
 function loadRefAttr(people) {
