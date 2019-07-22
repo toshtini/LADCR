df67438ffb217f9cc5b5c552e63f5ad75cde469b
Don Bates <39094801+dbates99@users.noreply.github.com>
2019-07-22 14:04:37 -0300

    db

--- SCRIPTS/EVENT/RCEA;~!~!~!~.js
+++ SCRIPTS/EVENT/RCEA;~!~!~!~.js
@@ -50,6 +50,7 @@ if (!publicUser)
 	addParameter(vEParams, "$$newSEStatus$$",afterEditSocialEquity);
 	if(afterEditSocialEquity != beforeEditSocialEquity)
 		{
+		editLookup ("LADCR_REFCONTACT_SOCIALEQUITY_STATUS", refContactSeqNo, afterEditSocialEquity)
 		sendNotification(null,refContactEmail,"","LACDR_SOCIAL_EQUITY_STATUS_CHANGE_ALERT",vEParams,null,capArray[0]); 
 		}
 	}
