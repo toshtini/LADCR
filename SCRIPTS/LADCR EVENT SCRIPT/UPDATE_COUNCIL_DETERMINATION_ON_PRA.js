a2da3d1d991e2248ff2ed3d42b81f5908bf00b85
Don Bates <39094801+dbates99@users.noreply.github.com>
2019-07-25 11:49:46 -0300

    db

---
+++ SCRIPTS/LADCR EVENT SCRIPT/UPDATE_COUNCIL_DETERMINATION_ON_PRA.js
@@ -0,0 +1,5 @@
+
+if(getAppSpecific("Retailer Commercial Cannabis Activity license in an area of Undue Concentration?") == "Yes" && balanceDue == 0)
+    {
+    updateAppStatus("Awaiting Council Determination","Updated from PRA")
+    }
