8cd009f607c0daf108c7708c50cbe4b4da82b2f9
Gregg Hess <ghess@accela.com>
2019-07-30 10:28:40 -0700

    Update ASA;LICENSES!~!~!APPLICATION.js
    Move from Dev

--- SCRIPTS/EVENT/ASA;LICENSES!~!~!APPLICATION.js
+++ SCRIPTS/EVENT/ASA;LICENSES!~!~!APPLICATION.js
@@ -16,9 +16,9 @@ if (!appMatch("Licenses/Cannabis/Testing/Application")) {
 
 // Assess fees
 if (AInfo["Is this a Renewal?"] && AInfo["Is this a Renewal?"].substr(0, 1).toUpperCase().equals("Y")) {
-	// skip fees
+	include("ASSESS_BUS_REN_FEES");
 } else {
-	include("ASSESS_BUS_APP_FEES");
+	include("ASSESS_BUS_APP_FEES2");
 }
 // end assess fees
 
