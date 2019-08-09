7523362edaec2984e33313600125820b01cb45f9
Gregg Hess <ghess@accela.com>
2019-08-09 09:17:45 -0700

    Update RCEA;~!~!~!~.js
    Added sendNotification2() for no associated records to contact

--- SCRIPTS/EVENT/RCEA;~!~!~!~.js
+++ SCRIPTS/EVENT/RCEA;~!~!~!~.js
@@ -1,4 +1,5 @@
 //07/17/2019 - ghess, added Salutation debug for non-public users
+//08/09/2017 - Don, sendNotification2() fix.
 if (publicUser) {
 	var people = aa.people.getPeople(ContactModel.getContactSeqNumber()).getOutput();
 	var seStatus = people.getSalutation();
@@ -38,7 +39,7 @@ if (!publicUser)
 	{
 	// If Agency updates the Social Equity Status email reference contact
 	var people = aa.people.getPeople(ContactModel.getContactSeqNumber()).getOutput();
-	var capArray = new Array;
+	var capArray = new Array; 
 	pSeqNumber = ContactModel.getContactSeqNumber()
 	pSeqNumber = aa.util.parseInt(pSeqNumber)
 	pSeqNumber = aa.util.parseLong(pSeqNumber)
@@ -52,7 +53,7 @@ if (!publicUser)
 	if(afterEditSocialEquity != beforeEditSocialEquity)
 		{
 		editLookup ("LADCR_REFCONTACT_SOCIALEQUITY_STATUS", ContactModel.getContactSeqNumber(), afterEditSocialEquity)
-		sendNotification(null,refContactEmail,"","LADCR_SOCIAL_EQUITY_STATUS_CHANGE_ALERT",vEParams,null,capArray[0]); 
+		sendNotification2(null,refContactEmail,"","LADCR_SOCIAL_EQUITY_STATUS_CHANGE_ALERT",vEParams,null); 
 		}
 	}
 
@@ -92,48 +93,33 @@ if(capArray.length > 0)
 		}
 	}
 
-function loadRefAttr(people) {
-var asiObj = null;
-var asi = new Array();    // associative array of attributes
-var customFieldsObj = null;
-var customFields = new Array();
-var customTablesObj = null;
-var customTables = new Array();
-
-//Attributes
-if (people.getAttributes() != null) {
-	asiObj = people.getAttributes().toArray();
-	if (asiObj != null) {
-		for (var xx1 in asiObj) {
-			logDebug("ATT : " + asiObj[xx1].attributeName + " = " + asiObj[xx1]);
-			asi[asiObj[xx1].attributeName] = asiObj[xx1];
-		}   
+	// now update any records Custom List with the reference contact identified in the Contact Sequence Number column
+	/*
+	var vAsyncScript = "SEND_ASITREFCONTACTUPDATE_ASYNC";
+	var envParameters = aa.util.newHashMap();
+	aa.env.setValue("table", "LIST OF OWNERS");
+	aa.env.setValue("subgroup", "CAN_BUS_APP");	
+	aa.env.setValue("column", "Contact Sequence Number");
+	aa.env.setValue("value", ContactModel.getContactSeqNumber());
+	aa.env.setValue("firstName", people.firstName.toString());
+	aa.env.setValue("lastName", people.lastName.toString());
+	aa.env.setValue("phone1", people.phone1.toString());
+	aa.env.setValue("email", people.email.toString());
+	aa.includeScript(vAsyncScript);
+	*/
+function sendNotification2(emailFrom,emailTo,emailCC,templateName,params,reportFile){
+	var result = null;
+	var id1 = ""
+	var id2 = ""
+	var id3 = ""
+	var capIDScriptModel = aa.cap.createCapIDScriptModel(id1, id2, id3);
+	result = aa.document.sendEmailAndSaveAsDocument(emailFrom, emailTo, emailCC, templateName, params, capIDScriptModel, reportFile);
+	if(result.getSuccess()){
+		logDebug("Sent email successfully!");
+		return true;
 	}
-}
-
-// contact ASI
-var tm = people.getTemplate();
-if (tm)	{
-	var templateGroups = tm.getTemplateForms();
-	var gArray = new Array();
-	if (!(templateGroups == null || templateGroups.size() == 0)) {
-		var subGroups = templateGroups.get(0).getSubgroups();
-		if (!(subGroups == null || subGroups.size() == 0)) {
-			for (var subGroupIndex = 0; subGroupIndex < subGroups.size(); subGroupIndex++) {
-				var subGroup = subGroups.get(subGroupIndex);
-				var fields = subGroup.getFields();
-				if (!(fields == null || fields.size() == 0)) {
-					for (var fieldIndex = 0; fieldIndex < fields.size(); fieldIndex++) {
-						var field = fields.get(fieldIndex);
-						logDebug("ASI : " + field.getDisplayFieldName() + " = " + field.getDefaultValue());
-						asi[field.getDisplayFieldName()] = field.getDefaultValue();
-					}
-				}
-			}
-		}
+	else{
+		logDebug("Failed to send mail. - " + result.getErrorType());
+		return false;
 	}
-}
-
-	
-	return asi;
 }
