3fe3221e29e2aa892e218a90a9c03c14a4b90720
grayquarter <john@grayquarter.com>
2019-08-20 09:51:00 -0700

    script 60

---
+++ SCRIPTS/LADCR EVENT SCRIPT/SEND_INVOICE.js
@@ -0,0 +1,10 @@
+// Script 60
+
+var vEParams = aa.util.newHashtable();
+addParameter(vEParams, "$$LicenseType$$", appTypeAlias);
+addParameter(vEParams, "$$ExpirationDate$$", dateAdd(null, 60));
+addParameter(vEParams, "$$ApplicationID$$", capIDString);
+var vRParams = aa.util.newHashtable();
+addParameter(vRParams, "p1Value", capIDString);
+emailContacts_BCC('Owner Applicant', "LADCR_OWNER_INVOICE", vEParams, "Invoice", vRParams);
+
