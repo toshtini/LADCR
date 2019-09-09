5eb6d3af05315bf2325e7ab34fbea756f287aba2
grayquarter <john@grayquarter.com>
2019-09-09 14:26:13 -0700

    missing func

---
+++ INCLUDES_CUSTOM/lacdUpdateAltID.js
@@ -0,0 +1,48 @@
+function lacdUpdateAltID(capIdToUpdate, recType, altId, ActivityType) {
+	var returnAltID;
+
+	if (recType == "PCN") {
+		// like LA-C-YY-######-TYPE-APP
+		var initialAltId = altId;
+		var activityLetter = ActivityType;
+		// Split out parts of the number
+		var LA = initialAltId.slice(0, 2)
+			var remainder = initialAltId.slice(4, 14);
+		var APP = initialAltId.slice(14, 18);
+		// Construct new Alt ID
+		returnAltID = LA + "-P" + remainder + "-" + activityLetter + APP;
+	}
+	if (recType == "NONPCN") {
+		// like LA-C-YY-######-TYPE-APP
+		var initialAltId = altId;
+		var activityLetter = ActivityType;
+		// Split out parts of the number
+		var LA = initialAltId.slice(0, 2)
+			var remainder = initialAltId.slice(4, 14);
+		var APP = initialAltId.slice(14, 18);
+		// Construct new Alt ID
+		var returnAltID = LA + "-C" + remainder + "-" + activityLetter + APP;
+	}
+	if (recType == "RENEWAL") {
+		// like LA-C-YY-######-TYPE-RENYY
+		var initialAltId = altId;
+		// Split out parts of the number
+		var todayDate = new Date();
+		var strYear = todayDate.getFullYear();
+		var strYear = strYear.toString();
+		var strYear = strYear.slice(2, 4);
+		var yearOfRen = strYear;
+		lenOfStr = initialAltId.length() - 3;
+		var AltIdSlice = initialAltId.slice(0, lenOfStr);
+		// Construct new Alt ID
+		var returnAltID = AltIdSlice + "REN" + yearOfRen;
+	}
+
+	if (returnAltID) {
+		aa.cap.updateCapAltID(capIdToUpdate, returnAltID);
+		logDebug("returnAltID is " + returnAltID);
+		// update global var
+		capIDString = returnAltID;
+	}
+
+}
