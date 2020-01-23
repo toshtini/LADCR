42475daf40ad088f815f7d277d0a1af2355dec20
grayquarter <john@grayquarter.com>
2020-01-23 09:44:08 -0800

    commit by emse tools 2020-01-23 09:44:05

--- SCRIPTS/EVENT/RCEB;~!~!~!~.js
+++ SCRIPTS/EVENT/RCEB;~!~!~!~.js
@@ -1,13 +1,38 @@
+/*
+
+// using expression for this now.
 if (publicUser) {
 	var people = aa.people.getPeople(ContactModel.getContactSeqNumber()).getOutput();
 	var seStatus = people.getSalutation();
-	
-	//if there is a status then dont allow saving
-	if (seStatus && !(seStatus=="" || seStatus==null)) {
-		cancel = true ; 
+	logDebug("seStatus is " + seStatus);
+	if (matches(seStatus,"Pending DCR Review","Not Eligible","SEP Tier 1 and Tier 2 Eligibility Verified","SEP Tier 2 Eligibility Verified","SEP Tier 1 Eligibility Verified")) {
+		cancel = true;
 		showMessage = true;
-		showDebug = false;
-		comment("You cannot make any changes after applying for Social Equity Verification. Please notify DCR for any updates.");
+		showDebug = false; // prevent debug
+		comment("No changes can be made to your profile at this time");
+		}
+}
 
+*/
+
+if (!publicUser)
+	{
+	var people = aa.people.getPeople(ContactModel.getContactSeqNumber()).getOutput();
+	var beforeEditSocialEquity = people.getSalutation();
+	if(beforeEditSocialEquity != null)
+		{
+		//update standard choice value for this reference contact to be read on the RCEA event to see if it has changed
+		var refContactSeqNo = ContactModel.getContactSeqNumber();
+		var refContactSEStatus = lookup("LADCR_REFCONTACT_SOCIALEQUITY_STATUS",refContactSeqNo)
+		// if found do an edit
+		if(refContactSEStatus != undefined)
+			{
+			editLookup ("LADCR_REFCONTACT_SOCIALEQUITY_STATUS", refContactSeqNo, beforeEditSocialEquity)
+			}
+		else
+		// if not do an add
+			{
+			addLookup ("LADCR_REFCONTACT_SOCIALEQUITY_STATUS", refContactSeqNo, beforeEditSocialEquity)
+			}
+		}
 	}
-} 
