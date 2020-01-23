7333d135aca27b8313bdb7f27aa593207de0558e
grayquarter <john@grayquarter.com>
2020-01-23 09:50:11 -0800

    commit by emse tools 2020-01-23 09:50:08

--- SCRIPTS/EVENT/PRA;LICENSES!~!~!APPLICATION.js
+++ SCRIPTS/EVENT/PRA;LICENSES!~!~!APPLICATION.js
@@ -10,6 +10,10 @@ include("UPDATE_APPLICATION_ISSUANCE");
 include("UPDATE_APPLICATION_ACCEPTANCE");
 // End script to move workflow task Application Acceptance to Submitted once payment has been recieved. Runs thr WTUA event.
 
-// Begin script to send local authorization notice. added 11/15/18
-include("SEND_LOCAL_AUTH_NOTICE");
+// Begin script to send local authorization notice. added 11/15/18, removed 12/13/2019 per Aaron
+//include("SEND_LOCAL_AUTH_NOTICE");
 // End script to send local authorization notice.
+ 
+// Begin script to update App Status on full payment for PCN type records to 'Awaiting Council Determination'
+include("UPDATE_COUNCIL_DETERMINATION_ON_PRA");
+// End script
