a85f4dc9361d4882c1d3db1b0e4c99798c32c724
Gregg Hess <ghess@accela.com>
2020-03-17 18:29:55 -0700

    Update CTRCA;LICENSES!~!~!APPLICATION.js
    commented out irrelevant code

--- SCRIPTS/EVENT/CTRCA;LICENSES!~!~!APPLICATION.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!~!~!APPLICATION.js
@@ -1,18 +1,18 @@
 // Insert the public user reference contact as the owner applicant
 //include("COPY_PUBLIC_USER_TO_OWNER_APPLICANT");
 // Insert the public user reference contact as the owner applicant or agent of service (ghess, 3/20/18)
-if (AInfo["Agent of Service"] == "Yes") {
-	include("COPY_PUBLIC_USER_TO_AGENT_OF_SERVICE");
-} else {
-	include("COPY_PUBLIC_USER_TO_OWNER_APPLICANT");
-}
+//if (AInfo["Agent of Service"] == "Yes") {
+//	include("COPY_PUBLIC_USER_TO_AGENT_OF_SERVICE");
+//} else {
+//	include("COPY_PUBLIC_USER_TO_OWNER_APPLICANT");
+//}
 
 // Begin script to set the Owner Applicant information in the Owners ASIT to Read Only.
-include("SET_OWNER_APPLICANT_OWNER_TABLE_READ_ONLY");
+//include("SET_OWNER_APPLICANT_OWNER_TABLE_READ_ONLY");
 // End script to set the Owner Applicant information in the Owners ASIT to Read Only.
 
 // Begin script to copy Business contact information (Business Name and Address) to record
-include("SAVE_BUSINESS_INFO_TO_RECORD");
+//include("SAVE_BUSINESS_INFO_TO_RECORD");
 // End script to copy Business contact information (Business Name and Address) to record
 
 //Begin script to invoice all fees and set workflow task Application Acceptance to Waiting for Payment when user Defers Payment in ACA
