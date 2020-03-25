440c8ef9bd1d874dd245c21b6b27550962f030f5
Gregg Hess <ghess@accela.com>
2020-03-25 12:45:32 -0700

    Update ASA;LICENSES!~!~!~.js

--- SCRIPTS/EVENT/ASA;LICENSES!~!~!~.js
+++ SCRIPTS/EVENT/ASA;LICENSES!~!~!~.js
@@ -6,9 +6,9 @@ if (!publicUser && !appMatch("*/*/*/Application")) {
 // End functionality to set inital workflow status.
 
 // Begin Story 5135, 6083 create document conditions of approval
-if (!publicUser) {
-	include("CREATE_DOCUMENT_CONDITIONS");
-}
+//if (!publicUser) {
+//	include("CREATE_DOCUMENT_CONDITIONS");
+//}
 // End Story 5135, 6083
 
 // Entity protoype
