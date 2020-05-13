fc53652b5290ef5c9d914a0b03c12875aceeb8e1
Gregg Hess <ghess@accela.com>
2020-05-13 15:29:23 -0700

    Update UPDATE_ACTIVITIES_FROM_ACA.js
    Edited for changes for Distribution Only

--- SCRIPTS/LADCR EVENT SCRIPT/UPDATE_ACTIVITIES_FROM_ACA.js
+++ SCRIPTS/LADCR EVENT SCRIPT/UPDATE_ACTIVITIES_FROM_ACA.js
@@ -1,29 +1,38 @@
 // This updates the original business activity application selections with the new ones used for drill down in ACA
-// Last Update 03/20/20
+// Last Update 05/13/20
 // This relies on AInfo[] being populated with custom fields.
 
 if (!isTrue(AInfo["Is this a Renewal?"])) {
 	logDebug("Not a Renewal");
 	// reset all fields
+	editAppSpecific("Adult Use", "N");
 	editAppSpecific("Adult-Use Cultivation Medium Indoor", "UNCHECKED");
 	editAppSpecific("Adult-Use Cultivation Small Indoor", "UNCHECKED");
 	editAppSpecific("Adult-Use Cultivation Specialty Indoor", "UNCHECKED");
 	editAppSpecific("Adult-Use Delivery Only", "UNCHECKED");
-	editAppSpecific("Adult-Use Distributor Transport Only", "UNCHECKED");
+	//editAppSpecific("Adult-Use Distributor Transport Only", "UNCHECKED");
 	editAppSpecific("Adult-Use Distributor", "UNCHECKED");
 	editAppSpecific("Adult-Use Manufacturer Level 1", "UNCHECKED");
 	editAppSpecific("Adult-Use Retail", "UNCHECKED");
+
+	editAppSpecific("Medical Use", "N");
 	editAppSpecific("Medical Cultivation Medium Indoor", "UNCHECKED");
 	editAppSpecific("Medical Cultivation Small Indoor", "UNCHECKED");
 	editAppSpecific("Medical Cultivation Specialty Indoor", "UNCHECKED");
 	editAppSpecific("Medical Delivery Only", "UNCHECKED");
-	editAppSpecific("Medical Distributor Transport Only", "UNCHECKED");
+	//editAppSpecific("Medical Distributor Transport Only", "UNCHECKED");
 	editAppSpecific("Medical Distributor", "UNCHECKED");
 	editAppSpecific("Medical Manufacturer Level 1", "UNCHECKED");
 	editAppSpecific("Medical Retail", "UNCHECKED");
+
+	editAppSpecific("Distribution Only","N");
 	editAppSpecific("Testing","N");
    
-  	if (String(AInfo["Use"]) == "Adult" ||  String(AInfo["Use"]) == "Adult and Medical") {
+   	if (String(AInfo["Use"]) == "Adult") {
+		editAppSpecific("Adult Use", "Y");
+	}
+
+ 	if (String(AInfo["Use"]) == "Adult" ||  String(AInfo["Use"]) == "Adult and Medical") {
 		logDebug("adult");
 	    if (isTrue(AInfo["Retail"])) {
 			editAppSpecific("Adult-Use Retail", "CHECKED");
@@ -44,14 +53,18 @@ if (!isTrue(AInfo["Is this a Renewal?"])) {
 	    if (isTrue(AInfo["Delivery Only"])) {
 			editAppSpecific("Adult-Use Delivery Only", "CHECKED");
 	    }
-	    if (isTrue(AInfo["Distribution Transport Only"])) {
-			editAppSpecific("Adult-Use Distributor Transport Only", "CHECKED");
-	    }
+	    //if (isTrue(AInfo["Distribution Transport Only"])) {
+		//	editAppSpecific("Adult-Use Distributor Transport Only", "CHECKED");
+	    //}
 	    if (isTrue(AInfo["Distributor"])) {
 			editAppSpecific("Adult-Use Distributor", "CHECKED");
 	    }
 	}
  
+   	if (String(AInfo["Use"]) == "Medical") {
+		editAppSpecific("Medical Use", "Y");
+	}
+
   	if (String(AInfo["Use"]) == "Medical" ||  String(AInfo["Use"]) == "Adult and Medical") {
 	    if (isTrue(AInfo["Retail"])) {
 			editAppSpecific("Medical Retail", "CHECKED");
@@ -71,15 +84,19 @@ if (!isTrue(AInfo["Is this a Renewal?"])) {
 	    if (isTrue(AInfo["Delivery Only"])) {
 			editAppSpecific("Medical Delivery Only", "CHECKED");
 	    }
-	    if (isTrue(AInfo["Distribution Transport Only"])) {
-			editAppSpecific("Medical Distributor Transport Only", "CHECKED");
-	    }
+	    //if (isTrue(AInfo["Distribution Transport Only"])) {
+		//	editAppSpecific("Medical Distributor Transport Only", "CHECKED");
+	    //}
 	    if (isTrue(AInfo["Distributor"])) {
 			editAppSpecific("Medical Distributor", "CHECKED");
 	    }
 	}
   
-  	if (String(AInfo["Use"]) == "Testing") {
+   	if (String(AInfo["Use"]) == "Distribution Only") {
+		editAppSpecific("Distribution Only","Y");
+	}
+
+ 	if (String(AInfo["Use"]) == "Testing") {
 		editAppSpecific("Testing","Y");
 	}
 }
