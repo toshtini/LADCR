3741509de945864962d6874a8329798dd1b4912c
grayquarter <john@grayquarter.com>
2020-05-20 14:46:17 -0700

    commit by emse tools 2020-05-20 14:46:13

--- SCRIPTS/LADCR EVENT SCRIPT/UPDATE_APP_SUPERVISOR_REVIEWS_COMPLETE.js
+++ SCRIPTS/LADCR EVENT SCRIPT/UPDATE_APP_SUPERVISOR_REVIEWS_COMPLETE.js
@@ -1,7 +1,7 @@
-//Begin Script #46 - When all parallel reviews are complete, set application status to “Pending Final Review”
+//Begin Script #46 - When all parallel reviews are complete, set application status to "Pending Final Review"
 if (matches(wfTask,"Supervisor Premises Review","Supervisor Owner Review","Supervisor Social Equity Review")) {
 	if (isTaskStatus("Supervisor Premises Review","Premises Review Complete") && isTaskStatus("Supervisor Owner Review","Owner Review Complete") && isTaskStatus("Supervisor Social Equity Review","Social Equity Review Complete")) {
 		updateAppStatus("Pending Final Review","Updated via script");
 	}
 }
-//End Script #46 - When all parallel reviews are complete, set application status to “Pending Final Review”
+//End Script #46 - When all parallel reviews are complete, set application status to "Pending Final Review"
