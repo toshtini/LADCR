6d1101ae8ce330a2a113cf6a8d860890dd7f96bd
grayquarter <john@grayquarter.com>
2020-05-22 09:01:06 -0700

    Update ACA_APP_PEOPLE_ONLOAD.js

--- SCRIPTS/PAGEFLOW/ACA_APP_PEOPLE_ONLOAD.js
+++ SCRIPTS/PAGEFLOW/ACA_APP_PEOPLE_ONLOAD.js
@@ -19,7 +19,7 @@
 |     changes are made, please add notes above.
 /------------------------------------------------------------------------------------------------------*/
 var showMessage = false; // Set to true to see results in popup window
-var showDebug = false; // Set to true to see debug messages in popup window
+var showDebug = true; // Set to true to see debug messages in popup window
 var useAppSpecificGroupName = false; // Use Group name when populating App Specific Info Values
 var useTaskSpecificGroupName = false; // Use Group name when populating Task Specific Info Values
 var cancel = false;
@@ -237,6 +237,7 @@ if (parentCapId){
 	aa.env.setValue("CapModel", cap);
 } else {
 	// add the ref contact from public userAgent
+	logDebug("Adding PublicUser as contact");
 	if (capId && "Y".equals(String(AInfo["Social Equity Requested"]).toUpperCase().substr(0,1))) {
 		addPublicUserToRecordAsContact(publicUserID, capId, "Social Equity Owner");
 	}
