477f7ecd59d501d87773f41c61f00ed9afc54718
grayquarter <john@grayquarter.com>
2020-07-01 08:37:01 -0700

    Update enable-disable-publicusers.js

--- SCRIPTS/MISC/enable-disable-publicusers.js
+++ SCRIPTS/MISC/enable-disable-publicusers.js
@@ -9,20 +9,19 @@ try {
 	// -- setting agency activation status to 'GOLIVE' so we can revert back only accounts that were active
 	// -- for testing accounts to work, they must be manually activated after this query runs
 	
-	
+	/*
 	var sql = "UPDATE XPUBLICUSER_SERVPROV SET STATUS = 'GOLIVE' " +
 		"WHERE STATUS='ACTIVE' AND " +
 		"SERV_PROV_CODE = 'LADCR'";
-	
+	*/
 	
 	// re-enable all ACA users for LADCR agency
 	// -- only accounts that were previously active will be activated
 
-	/*
+	
 	var sql = "UPDATE XPUBLICUSER_SERVPROV SET STATUS = 'ACTIVE' " +
 		"WHERE STATUS='GOLIVE' AND " +
 		"SERV_PROV_CODE = 'LADCR'";
-	*/
 	
 	if (sql) {		
 		doSQL(sql);
@@ -39,40 +38,16 @@ catch (err) {
 
 function doSQL(sql) {
 
-	try {
-		var array = [];
-		var initialContext = aa.proxyInvoker.newInstance("javax.naming.InitialContext", null).getOutput();
-		var ds = initialContext.lookup("java:/AA");
-		var conn = ds.getConnection();
-		var sStmt = conn.prepareStatement(sql);
-
-		if (sql.toUpperCase().indexOf("SELECT") == 0) {
-			aa.print("(doSQL) executing : " + sql);
-			var rSet = sStmt.executeQuery();
-			while (rSet.next()) {
-				var obj = {};
-				var md = rSet.getMetaData();
-				var columns = md.getColumnCount();
-				for (i = 1; i <= columns; i++) {
-					obj[md.getColumnName(i)] = String(rSet.getString(md.getColumnName(i)));
-				}
-				obj.count = rSet.getRow();
-				array.push(obj);
-			}
-			aa.print("(doSQL) number of rows returned : " + array.length);
-			aa.print(JSON.stringify(array));
-			rSet.close();
-		} else {
-			aa.print("(doSQL) executing : " + sql);
-			var r = sStmt.executeUpdate();
-			aa.print("(doSQL) number of rows affected : " + r);
-		}
-
-		sStmt.close();
-		conn.close();
-	} catch (err) {
-		aa.print(err.message);
-	}
+var dq = aa.db.update(sql, []);
+if (dq.getSuccess()) {
+    var dso = dq.getOutput();
+    if (dso) {
+        aa.print(dso);
+    }
+}
+else{
+    aa.print("error " + dq.getErrorMessage());
+}
 }
 
 
