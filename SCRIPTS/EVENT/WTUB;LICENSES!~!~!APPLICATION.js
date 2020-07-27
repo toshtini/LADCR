9f1e204400f4826147b8516607180a61e5ac678c
Gregg Hess <ghess@accela.com>
2020-07-27 08:56:28 -0700

    Update WTUB;LICENSES!~!~!APPLICATION.js
    Commenting out CHECK_OWNER_APPLICANT

--- SCRIPTS/EVENT/WTUB;LICENSES!~!~!APPLICATION.js
+++ SCRIPTS/EVENT/WTUB;LICENSES!~!~!APPLICATION.js
@@ -11,7 +11,7 @@ include("CHECK_TEMPORARY_REQUESTED");
 // End script to check if a temporary license has not been requested or previously issued and prevent if either is yes
 
 // Begin script to check for an Owner Applicant prior to any workflow progress
-include("CHECK_OWNER_APPLICANT");
+//include("CHECK_OWNER_APPLICANT");
 // End script to check for an Owner Applicant prior to any workflow progress
 
 // Begin script to check for any outstanding conditons of approval
