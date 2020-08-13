b10cbf878369d94e77cda4eb6b3754527b7d98a3
grayquarter <john@grayquarter.com>
2020-08-13 16:59:37 -0700

    commit by emse tools 2020-08-13 18:59:35

--- SCRIPTS/EVENT/CTRCA;LICENSES!~!~!APPLICATION.js
+++ SCRIPTS/EVENT/CTRCA;LICENSES!~!~!APPLICATION.js
@@ -1,18 +1,18 @@
 // Insert the public user reference contact as the owner applicant
 //include("COPY_PUBLIC_USER_TO_OWNER_APPLICANT");
 // Insert the public user reference contact as the owner applicant or agent of service (ghess, 3/20/18)
-if (AInfo["Agent of Service"] == "Yes") {
-	include("COPY_PUBLIC_USER_TO_AGENT_OF_SERVICE");
-} else {
-	//include("COPY_PUBLIC_USER_TO_OWNER_APPLICANT"); //for cleanup 3/25/2020
-}
+//if (AInfo["Agent of Service"] == "Yes") {
+//	include("COPY_PUBLIC_USER_TO_AGENT_OF_SERVICE");
+//} else {
+//	include("COPY_PUBLIC_USER_TO_OWNER_APPLICANT");
+//}
 
 // Begin script to set the Owner Applicant information in the Owners ASIT to Read Only.
-include("SET_OWNER_APPLICANT_OWNER_TABLE_READ_ONLY");
+//include("SET_OWNER_APPLICANT_OWNER_TABLE_READ_ONLY");
 // End script to set the Owner Applicant information in the Owners ASIT to Read Only.
 
 // Begin script to copy Business contact information (Business Name and Address) to record
-include("SAVE_BUSINESS_INFO_TO_RECORD");
+//include("SAVE_BUSINESS_INFO_TO_RECORD");
 // End script to copy Business contact information (Business Name and Address) to record
 
 //Begin script to invoice all fees and set workflow task Application Acceptance to Waiting for Payment when user Defers Payment in ACA
@@ -26,20 +26,36 @@ if (publicUser) {
 }
 // End functionality to set inital workflow status.
 
+// Begin script to update the Business Activities selected in ACA
+if(publicUser && appMatch("Licenses/Cannabis/Business/Application")){
+	include("UPDATE_ACTIVITIES_FROM_ACA");
+}
+// End script to update the Business Activities selected in ACA
+
 // Begin script to update the Application AltID based on Business Activity
-//if(publicUser && appMatch("Licenses/Cannabis/Business/Application")){
-//	include("UPDATE_APPLICATION_MJ_ALTID");
-//}
+if(publicUser && appMatch("Licenses/Cannabis/Business/Application")){
+	include("UPDATE_APPLICATION_MJ_ALTID");
+}
 // End script to update the Application AltID based on Business Activity
+
+// Begin script to set renewal parent and child application status
+if(publicUser && appMatch("Licenses/Cannabis/Business/Application")){
+	include("SET_APP_RENEWAL_STATUSES");
+}
+// End script to set renewal parent and child application status
 
 // Begin script to update the Application field Social Equity Applicants Reference Contact ID with the public users refcontactno
 if(publicUser && appMatch("Licenses/Cannabis/Business/Application")){
 	include("ACA_SAVE_PUBLICUSER_REFCONTACT_REFNO_TO_APP");
 }
 
-// script 60, 8/29/2019, per Jason, invoice not ready to distribute. Commenting out code.
-//if(AInfo["Retailer Commercial Cannabis Activity license in an area of Undue Concentration?"] == "Yes"){
-//	include("SEND_INVOICE");
-//}
-
+// script 60, 8/29/2019
+if(AInfo["Is this a Renewal?"] != "Yes" && AInfo["Retailer Commercial Cannabis Activity license in an area of Undue Concentration?"] != "Yes"){
+	include("SEND_INVOICE");
+}
 // End script
+
+// use the address to populate data from ZIMAS layers
+if(publicUser){
+	include("POPULATE_ZIMAS_DATA");
+}
