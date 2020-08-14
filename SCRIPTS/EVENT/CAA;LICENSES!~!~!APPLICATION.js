87d0fbe604fa5f3ab85c84de7ed7af1620df5877
grayquarter <john@grayquarter.com>
2020-08-13 17:00:37 -0700

    commit by emse tools 2020-08-13 19:00:36

--- SCRIPTS/EVENT/CAA;LICENSES!~!~!APPLICATION.js
+++ SCRIPTS/EVENT/CAA;LICENSES!~!~!APPLICATION.js
@@ -1,3 +1,6 @@
 // Begin script to copy Owner Applicant information to the Owners ASIT.
 include("SAVE_OWNER_APPLICANT_TO_OWNER_TABLE");
 // End script to copy Owner Applicant information to the Owners ASIT.
+
+// update contact permissions in ACA
+include("UPDATE_ACA_VISIBILITY_BY_CONTACT");
