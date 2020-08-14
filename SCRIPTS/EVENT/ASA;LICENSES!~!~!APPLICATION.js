ef5bbc9fc2b391224e0f690e6d5a6fd486fe7e08
grayquarter <john@grayquarter.com>
2020-08-13 17:02:21 -0700

    commit by emse tools 2020-08-13 19:02:20

--- SCRIPTS/EVENT/ASA;LICENSES!~!~!APPLICATION.js
+++ SCRIPTS/EVENT/ASA;LICENSES!~!~!APPLICATION.js
@@ -5,55 +5,33 @@
 // End associated forms for owner data
 
 // Begin script to copy Business contact information (Business Name and Address) to record
-include("SAVE_BUSINESS_INFO_TO_RECORD");
+//include("SAVE_BUSINESS_INFO_TO_RECORD"); //3/17/2020 Business contact type no longer valid 
 // End script to copy Business contact information (Business Name and Address) to record
 
 // Check matching FEIN or SSN for this BTRC, not for testing records
-if (!appMatch("Licenses/Cannabis/Testing/Application")) {
-	include("CHECK_SSN_FEIN_MATCH");
-}
+//if (!appMatch("Licenses/Cannabis/Testing/Application")) {
+//	include("CHECK_SSN_FEIN_MATCH");
+//}
 // end check for fein/ssn
 
-// Assess fees - commented out for temp renewal processing 3/24/2020
-//if (AInfo["Is this a Renewal?"] && AInfo["Is this a Renewal?"].substr(0, 1).toUpperCase().equals("Y")) {
-//	include("ASSESS_BUS_REN_FEES");
-//} else {
-//	include("ASSESS_BUS_APP_FEES2");
-//}
+// Assess fees
+if (AInfo["Is this a Renewal?"] && AInfo["Is this a Renewal?"].substr(0, 1).toUpperCase().equals("Y")) {
+	//include("ASSESS_BUS_REN_FEES"); //use the ame for now 1/30/2020
+	//include("ASSESS_BUS_APP_FEES");
+} else {
+	//include("ASSESS_BUS_APP_FEES2");
+	//include("ASSESS_BUS_APP_FEES");
+	include("ASSESS_BUS_APP_FEES_2020.06");
+}
 // end assess fees
 
 // Begin script to update the Application AltID based on Business Activity
-//if(!publicUser && appMatch("Licenses/Cannabis/Business/Application")){
-//	include("UPDATE_APPLICATION_MJ_ALTID");
-//}
+if(!publicUser && appMatch("Licenses/Cannabis/Business/Application")){
+	include("UPDATE_APPLICATION_MJ_ALTID");
+}
 // End script to update the Application AltID based on Business Activity
 
-//Start Core Renewal Functionality 03/25/2020
-if (parentCapId == "undefined" || parentCapId == null) {
-	parentCapId = aa.env.getValue("ParentCapID");
+// use the address to populate data from ZIMAS layers
+if(!publicUser){
+	include("POPULATE_ZIMAS_DATA");
 }
-
-if (parentCapId != null) {
-		
-	//Copy Parcels from license to renewal
-	copyParcels(parentCapId,capId);
-	
-	//Copy addresses from license to renewal
-	copyAddress(parentCapId,capId);
-	
-	//copy ASI Info from license to renewal
-	copyASIInfo(parentCapId,capId);
-
-	//Copy ASIT from license to renewal (exclude Permit Information)
-	copyASITables(parentCapId,capId);
-
-	//Copy Contacts from license to renewal
-	copyContacts3_0(parentCapId,capId);
-	
-	//Copy Work Description from license to renewal
-	aa.cap.copyCapWorkDesInfo(parentCapId,capId);
-
-	//Copy application name from license to renewal
-	editAppName(getAppName(parentCapId),capId);
-}
-//End Core Renewal Functionality
