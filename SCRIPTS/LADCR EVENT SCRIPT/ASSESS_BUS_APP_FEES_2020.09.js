d3d0a088e017b5bddc2edf5d44153805a4003fef
Gregg Hess <ghess@accela.com>
2020-09-04 16:34:38 -0700

    Create ASSESS_BUS_APP_FEES_2020.09.js
    Initial create

---
+++ SCRIPTS/LADCR EVENT SCRIPT/ASSESS_BUS_APP_FEES_2020.09.js
@@ -0,0 +1,15 @@
+//ASSESS_BUS_APP_FEES_2020.09.js
+// Uses business activity application drill down selections in ACA
+// Last Update 09/04/20
+// This relies on AInfo[] being populated with custom fields.
+
+if (!isTrue(AInfo["Is this a Renewal?"])) {
+	logDebug("Not a Renewal");
+ 
+	updateFee("J300","CAN_BUS_APP","FINAL",1,"Y"); //Cannabis Pre-Application Review Fee
+
+}
+
+function isTrue(o) {
+	return String(o) == "CHECKED" || String(o) == "YES" || String(o) == "Yes";
+}
