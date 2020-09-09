da8d876c0ae5d7417ba5ade8ec9b53a6bbca061e
Gregg Hess <ghess@accela.com>
2020-09-09 16:09:30 -0700

    Update ASSESS_BUS_APP_FEES_2020.09.js
    Added fee J098B21

--- SCRIPTS/LADCR EVENT SCRIPT/ASSESS_BUS_APP_FEES_2020.09.js
+++ SCRIPTS/LADCR EVENT SCRIPT/ASSESS_BUS_APP_FEES_2020.09.js
@@ -1,15 +1,19 @@
 //ASSESS_BUS_APP_FEES_2020.09.js
 // Uses business activity application drill down selections in ACA
-// Last Update 09/04/20
+// Last Update 09/09/20
 // This relies on AInfo[] being populated with custom fields.
+
+if (isTrue(AInfo["Are you submitting a Modification Request?"])) {
+	updateFee("J098B21","CAN_BUS_APP","FINAL",1,"Y"); //Cannabis Application Review Fee
+}
 
 if (!isTrue(AInfo["Is this a Renewal?"])) {
 	logDebug("Not a Renewal");
  
-	//updateFee("J300","CAN_BUS_APP","FINAL",1,"Y"); //Cannabis Pre-Application Review Fee
-	updateFee("F100","CAN_BUS_APP","FINAL",1,"Y"); //place holder until fee is created
+	updateFee("J300","CAN_BUS_APP","FINAL",1,"Y"); //Cannabis Pre-Application Review Fee
 
 }
+
 
 function isTrue(o) {
 	return String(o) == "CHECKED" || String(o) == "YES" || String(o) == "Yes";
