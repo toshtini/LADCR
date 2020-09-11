90201499fcc1081176354c1e5835935474b62d43
Gregg Hess <ghess@accela.com>
2020-09-11 09:36:59 -0700

    Update SET_APP_RENEWAL_STATUSES.js
    Add check for "Y"

--- SCRIPTS/LADCR EVENT SCRIPT/SET_APP_RENEWAL_STATUSES.js
+++ SCRIPTS/LADCR EVENT SCRIPT/SET_APP_RENEWAL_STATUSES.js
@@ -1,7 +1,7 @@
 // SET_APP_RENEWAL_STATUSES
 // Set renewal parent and child initial record statuses
 // Last update: 05/13/2020
-if(getAppSpecific("Is this a Renewal?") == "Yes") {
+if(getAppSpecific("Is this a Renewal?") == "Yes" || getAppSpecific("Is this a Renewal?") == "Y") {
 	updateAppStatus("Temporarily Approved","Initial Status",capId);
 	updateAppStatus("Deprecated","Renewal Update",parentCapId);
 }
