7710b219a4e580ee8fa347f03c2605db3e1da458
Gregg Hess <ghess@accela.com>
2020-09-29 16:04:16 -0700

    Update ASSESS_BUS_APP_FEES_2020.10.js
    added code

--- SCRIPTS/LADCR EVENT SCRIPT/ASSESS_BUS_APP_FEES_2020.10.js
+++ SCRIPTS/LADCR EVENT SCRIPT/ASSESS_BUS_APP_FEES_2020.10.js
@@ -1,1 +1,12 @@
 //ASSESS_BUS_APP_FEES_2020.10.js
+// Last Update 09/29/20
+// This relies on AInfo[] being populated with custom fields.
+
+if (!isTrue(AInfo["Is this a Renewal?"])) {
+    logDebug("Not a Renewal");
+    updateFee("J300", "CAN_BUS_APP", "FINAL", 1, "Y"); //Cannabis Pre-Application Review Fee
+} 
+
+function isTrue(o) {
+    return String(o) == "CHECKED" || String(o) == "YES" || String(o) == "Yes" || String(o) == "Y";
+}
